1b917c23310423fa6425e6a1ec092a0a37750053
Makarand <mbauskar@gmail.com>
2015-06-09 11:31:23 +0530

    reset the markup if ignore pricing rule is checked

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -127,6 +127,14 @@ def get_pricing_rule_for_item(args):
 	})
 
 	if args.ignore_pricing_rule or not args.item_code:
+		# [TA]
+		# If Ignore Pricing Rule is checked then set the fields to defaults
+		if item_details.doctype in ["Quotation Item"]:
+			item_details.update({
+				"type":"Percentage",
+				"rate_or_amount": 0.00
+				})
+		# [TA]
 		return item_details
 
 	if not (args.item_group and args.brand):

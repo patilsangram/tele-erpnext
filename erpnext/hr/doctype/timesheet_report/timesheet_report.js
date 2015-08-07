0188428f2319ec528b3270ea1d91078959789650
Makarand <mbauskar@gmail.com>
2015-08-07 11:16:11 +0530

    on set the from date and to date value

--- erpnext/hr/doctype/timesheet_report/timesheet_report.js
+++ erpnext/hr/doctype/timesheet_report/timesheet_report.js
@@ -1,6 +1,6 @@
 cur_frm.cscript.onload = function(doc){
     doc.employee = "";
-    doc.from_date = "";
-    doc.to_date = "";
+    // doc.from_date = "";
+    // doc.to_date = "";
     doc.html_code = "";
 }
